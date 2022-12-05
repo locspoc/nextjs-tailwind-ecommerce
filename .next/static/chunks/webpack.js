dedb3b47efe60733ff90bd37cb88d8fdae8f68ce
Loc Tran <mrlocspoc@gmail.com>
2022-12-05 21:31:12 +1030

    10. Handle Add To Cart complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "187a147decb95165"; }
+/******/ 		__webpack_require__.h = function() { return "830fa9105d500db5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,17 +1,38 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
+	const { state, dispatch } = useContext(Store);
 	const { query } = useRouter();
 	const { slug } = query;
 	const product = data.products.find((x) => x.slug === slug);
+
 	if (!product) {
 		return <div>Product Not Found</div>;
 	}
+
+	const addToCartHandler = () => {
+		const existItem = state.cart.cartItems.find(
+			(x) => x.slug === product.slug
+		);
+		const quantity = existItem ? existItem.quantity + 1 : 1;
+
+		if (product.countInStock < quantity) {
+			alert('Sorry. Product is out of stock');
+			return;
+		}
+
+		dispatch({
+			type: 'CART_ADD_ITEM',
+			payload: { ...product, quantity: quantity },
+		});
+	};
+
 	return (
 		<Layout title={product.name}>
 			<div className="py-2">
@@ -55,7 +76,10 @@ export default function ProductScreen() {
 							</div>
 						</div>
 					</div>
-					<button className="primary-button w-full">
+					<button
+						className="primary-button w-full"
+						onClick={addToCartHandler}
+					>
 						Add To Cart
 					</button>
 				</div>
